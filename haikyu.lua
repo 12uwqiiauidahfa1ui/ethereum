a4a7fd97f58a0e6c425ddea86dd24f684e52d1dd
12uwqiiauidahfa1ui <pro061314@gmail.com>
2025-01-28 00:35:00 +0800

    Update haikyu.lua

--- haikyu.lua
+++ haikyu.lua
@@ -176,7 +176,7 @@ local VirtualInputManager = game:GetService("VirtualInputManager")
 
 local function pressEscTwice()
     -- First press with delay of 0.3 seconds
-    task.wait(23)
+    task.wait(28)
 
     VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Escape, false, game)  -- Key down
     VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Escape, false, game) -- Key up
